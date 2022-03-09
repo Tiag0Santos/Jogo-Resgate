0192b1cd02768b269fbf74144097e6a6f1c64237
Tiag0Santos <titaods@gmail.com>
2022-03-09 10:20:43 -0300

    adicionando movimentacao as teclas

--- js/js.js
+++ js/js.js
@@ -10,6 +10,24 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {}
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+		jogo.pressionou = [];
+
+	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+	
+	
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
 	
 	//Game Loop
 
@@ -18,10 +36,11 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+	movejogador();
 	
 	} // Fim da função loop()
 
-//Função que movimenta o fundo do jogo
+	//Função que movimenta o fundo do jogo
 	
 	function movefundo() {
 	
@@ -29,6 +48,27 @@ function start() { // Inicio da função start()
 	$("#fundoGame").css("background-position",esquerda-1);
 	
 	} // fim da função movefundo()
+
+	function movejogador() {
+	
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+		
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {
+			
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);	
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			
+			//Chama função Disparo	
+		}
+	
+		} // fim da função movejogador()
 }
 
 inicio.addEventListener('click', start);
